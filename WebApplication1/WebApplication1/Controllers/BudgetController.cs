5d305debec6993facaf56b48a12767d420d49105
Zhuludkovr <rzhuludkov@bk.ru>
2021-09-19 17:52:43 +0300

    db
    Мы создали сервер

---
+++ WebApplication1/WebApplication1/Controllers/BudgetController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BudgetController : ControllerBase
+    {
+        private readonly BudgetContext _context;
+
+        public BudgetController(BudgetContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Budget
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Budget>>> GetBudgets()
+        {
+            return await _context.Budgets.ToListAsync();
+        }
+
+        // GET: api/Budget/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Budget>> GetBudget(int id)
+        {
+            var budget = await _context.Budgets.FindAsync(id);
+
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            return budget;
+        }
+
+        // PUT: api/Budget/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBudget(int id, Budget budget)
+        {
+            if (id != budget.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(budget).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BudgetExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Budget
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Budget>> PostBudget(Budget budget)
+        {
+            _context.Budgets.Add(budget);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBudget", new { id = budget.Id }, budget);
+        }
+
+        // DELETE: api/Budget/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Budget>> DeleteBudget(int id)
+        {
+            var budget = await _context.Budgets.FindAsync(id);
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            _context.Budgets.Remove(budget);
+            await _context.SaveChangesAsync();
+
+            return budget;
+        }
+
+        private bool BudgetExists(int id)
+        {
+            return _context.Budgets.Any(e => e.Id == id);
+        }
+    }
+}
